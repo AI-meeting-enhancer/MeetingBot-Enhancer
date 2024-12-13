a61daa1c1ac6dd1e49328579f143d3aec86a4f1f
AI-meeting-enhancer <yaskivartur0830@gmail.com>
2024-12-13 06:54:36 -0500

    added meeting transcription modification part

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = Config.GOOGLE_APPLICATION_CREDENTIALS
 
 from audio.audio_stream import stream_audio_to_text
-# from transcription.modify_transcription import modify_transcription
+from transcription.modify_transcription import modify_transcription
 
 
 # Flag to control the transcription thread
@@ -20,10 +20,8 @@ def start_real_time_transcription():
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description="A simple command line argument parser.")
-    # parser.add_argument('-o', '--outputFileName', type=str, required=True, help="Temp file name to store meeting transcription.")
-    # parser.add_argument('-s', '--socketPath', type=str, help="dir name for unix socket")
     parser.add_argument('-t', '--translatorApi', type=str, required=True, help="API abbriviation for transcription among 'g':Google stt api, 'd':Deepgram api.")
-    # parser.add_argument('-m', '--modification', type=bool, help="if you set this argument, this script will work for only modification")
+    parser.add_argument('-m', '--modification', type=bool, help="if you set this argument, this script will work for only modification")
 
     args = parser.parse_args()
     
@@ -34,9 +32,9 @@ if __name__ == "__main__":
         Config.SOCKET_PATH = './bots/zoom_bot/sock/deepgramApi/meeting.sock'
         Config.OUTPUT_FILE = './tmp/meeting_temp_deepgram.txt'
 
-    # if args.modification:
-    #     modify_transcription()
-    #     exit(0)
+    if args.modification:
+        modify_transcription()
+        exit(0)
 
     transcription_thread = threading.Thread(target=start_real_time_transcription)
     transcription_thread.start()
